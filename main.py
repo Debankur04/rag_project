9d125cbe57e8971d97cadf4af639e46421c58c6f
Debankur04 <debankurdutta04@gmail.com>
2026-08-13 23:10:10 +0530

    added cors

--- main.py
+++ main.py
@@ -39,9 +39,10 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://poneglyph-sigma.vercel.app/"
+        "https://poneglyph-sigma.vercel.app",
+        "https://poneglyph.debankurdutta.me",
     ],
-    allow_credentials = True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
